0ce79a21c3f236c0012c1d26b3a839682836fc85
emre <emrecorek@gmail.com>
2022-07-18 22:03:15 +0300

    initial commit

---
+++ src/test/java/com/upgenix/runners/CukesRunner.java
@@ -0,0 +1,30 @@
+package com.upgenix.runners;
+
+
+import com.upgenix.step_definitions.Hooks;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+
+@CucumberOptions(
+
+        plugin = {
+                //pretty,
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/upgenix/step_definitions",
+        dryRun = false,
+        tags = "@UPGNX-313"
+
+
+)
+
+public class CukesRunner {
+}
---
+++ src/test/java/com/upgenix/utilities/WaitUtils.java
@@ -0,0 +1,17 @@
+package com.upgenix.utilities;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtils {
+
+
+    public static void waitVisibilityOfElement(WebElement element){
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+}
